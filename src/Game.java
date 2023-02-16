7329e6e10539bab92b816e90552337b7babc3f85
sagarsomwanshi <sagarsomwanshi13@gmail.com>
2023-02-16 13:19:57 +0530

    Added : Sets position to Zero

---
+++ src/Game.java
@@ -0,0 +1,11 @@
+public class Game {
+    static int position;
+
+    public static void main(String[] args) {
+        UC1();
+
+    }
+    public static void UC1(){
+        position = 0;
+    }
+}
