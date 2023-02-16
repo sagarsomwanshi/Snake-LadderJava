bb6bf8963b33395ebbe30da3c9245e7a6e7bf50e
sagarsomwanshi <sagarsomwanshi13@gmail.com>
2023-02-16 14:42:26 +0530

    Added : Checks for option No play, Ladder, Snake Randomly

--- src/Game.java
+++ src/Game.java
@@ -2,18 +2,36 @@ public class Game {
     static int position;
     int Dice;
     public static void main(String[] args) {
-        UC1();
-        UC2();
+        UC3();
 
     }
     public static void UC1(){
         position = 0;
     }
 
-    public static void UC2(){
+    public static int UC2(){
         Game G = new Game();
             G.Dice = (int) ((Math.random() * 10) % 6 + 1);
-            System.out.println(G.Dice);
+            return G.Dice;
+    }
+    public static void UC3(){
+        int Move = (int) ((Math.random() * 10) % 3);
+        if(Move == 0){
+            System.out.println("No PLay");
+        } else if (Move == 1) {
+            System.out.println("Ladder");
+            int dice = UC2();
+            System.out.println("Dice : " +dice);
+            position = position + dice;;
+            System.out.println("Your Position : " +position);
+        }else {
+            System.out.println("Snake");
+            int dice = UC2();
+            position = position - dice;
+            System.out.println("Dice : " +dice);
+
+            System.out.println("Your Position : " +position);
+        }
     }
 }
 
