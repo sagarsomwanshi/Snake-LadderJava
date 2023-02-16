c552e293aaa1c86be68b6ae2969a62b1882f6658
sagarsomwanshi <sagarsomwanshi13@gmail.com>
2023-02-16 14:15:48 +0530

    Added: Rolls the dice & gives random number from 1-6

--- src/Game.java
+++ src/Game.java
@@ -1,11 +1,19 @@
 public class Game {
     static int position;
-
+    int Dice;
     public static void main(String[] args) {
         UC1();
+        UC2();
 
     }
     public static void UC1(){
         position = 0;
     }
+
+    public static void UC2(){
+        Game G = new Game();
+            G.Dice = (int) ((Math.random() * 10) % 6 + 1);
+            System.out.println(G.Dice);
+    }
 }
+
